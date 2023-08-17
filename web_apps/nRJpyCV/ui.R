41ae324ef8e48cc5614b9c126d4eaa2822c6e207
kyshakir@tva.gov <kyshakir@tva.gov>
2023-08-17 14:47:21 +0000

    Saved web app PairsVisual (KSHAKIR_TESTZIP.nRJpyCV)

--- web_apps/nRJpyCV/ui.R
+++ web_apps/nRJpyCV/ui.R
@@ -4,16 +4,11 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Hello Shiny!"),
+  titlePanel("Pairs Visual"),
 
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
     ),
 
     # Show a plot of the generated distribution
